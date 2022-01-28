4a21118d4d0ea6545d0b66eb6a6edef98bd1bf07
Tim Inge |th <t.hoermann@audivir.de>
2022-01-28 16:35:06 +0100

    Create flashlight.rb

---
+++ Casks/flashlight.rb
@@ -0,0 +1,12 @@
+cask "flashlight" do
+  #version "2.7.1_387"
+  #sha256 "a3738d0513d736918a6d71535ef3d85dd184af267c05698e49ac4c6b48f38e17"
+
+  url "http://flashlight.nateparrott.com/latest_download"
+  name "Flashlight"
+  desc "Search the web, save a note, add a reminder, or do over 200 other things, just by typing into your Mac's Spotlight search box"
+  homepage "http://flashlight.nateparrott.com/"
+
+  app "Flashlight.app"
+  #app "Alfred 2.app/Contents/Preferences/Alfred Preferences.app"
+end

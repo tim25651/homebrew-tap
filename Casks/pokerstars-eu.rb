caf631306bd0877193dbc4c622ee96a2eb79f44b
Tim Inge |th <t.hoermann@audivir.de>
2022-01-28 17:10:52 +0100

    Update pokerstars-eu.rb

--- Casks/pokerstars-eu.rb
+++ Casks/pokerstars-eu.rb
@@ -1,5 +1,5 @@
 cask "pokerstars-eu" do
-  version "latest"
+  version "PokerStarsEU"
   #sha256 "a3738d0513d736918a6d71535ef3d85dd184af267c05698e49ac4c6b48f38e17"
 
   url "https://download.pokerstars.de/poker/client/download/"
@@ -7,6 +7,6 @@ cask "pokerstars-eu" do
   desc "PokerStars ist die Heimat der spannendsten Pokerspiele"
   homepage "https://www.pokerstars.eu/"
 
-  app "PokerStarsEU/PokerStarsEU.app"
+  app "PokerStarsEU.app"
   #app "Alfred 2.app/Contents/Preferences/Alfred Preferences.app"
 end

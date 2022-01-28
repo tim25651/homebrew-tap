9e61a686cbe8df8032be95d5c0dff6f4b252d7ae
Tim Inge |th <t.hoermann@audivir.de>
2022-01-28 16:55:12 +0100

    Update sky-ticket.rb

--- Casks/sky-ticket.rb
+++ Casks/sky-ticket.rb
@@ -1,1 +1,12 @@
-https://web.static.nowtv.com/watch/player/skyticket/de/latest/SkyTicket-Mac.dmg
+cask "sky-ticket" do
+  version "latest"
+  #sha256 "a3738d0513d736918a6d71535ef3d85dd184af267c05698e49ac4c6b48f38e17"
+
+  url "https://web.static.nowtv.com/watch/player/skyticket/de/latest/SkyTicket-Mac.dmg"
+  name "Sky Ticket"
+  desc "Streame exklusive Top-Serien, aktuelle Blockbuster und besten Live-Sport"
+  homepage "https://skyticket.sky.de/home/kunde"
+
+  app "Sky Ticket.app"
+  #app "Alfred 2.app/Contents/Preferences/Alfred Preferences.app"
+end

55b3922cd746932a70e4c6aecf9539525d2f9974
Tim Inge |th <t.hoermann@audivir.de>
2023-02-18 12:33:07 +0100

    Update chimerax.rb

--- Casks/chimerax.rb
+++ Casks/chimerax.rb
@@ -2,7 +2,7 @@ cask "chimerax" do
   version "1.5"
   #sha256 "a3738d0513d736918a6d71535ef3d85dd184af267c05698e49ac4c6b48f38e17"
 
-  url "~/Downloads/ChimeraX-#{version}.dmg"
+  url "file:///Users/tim/OneDownloads/ChimeraX-#{version}.dmg"
   name "ChimeraX"
   desc "UCSF ChimeraX (or simply ChimeraX) is the next-generation molecular visualization program from the Resource for Biocomputing, Visualization, and Informatics (RBVI)"
   homepage "https://www.cgl.ucsf.edu/chimerax/"
